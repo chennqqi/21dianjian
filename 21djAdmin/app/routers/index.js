1d45d38f0846c565e367a7147cb6933e08f2f69e
fujinliang <3487662289@qq.com>
2017-08-07 16:05:33 +0800

    router 添加

--- 21djAdmin/app/routers/index.js
+++ 21djAdmin/app/routers/index.js
@@ -2,8 +2,13 @@ const router = require('koa-router')()
 
 const api = require('./api/index')
 const admin = require('./admin/index')
+const wxApi = require('./wxApi/index')
 
+router.get('/', function(ctx) {
+    ctx.body = 'ok'
+})
 router.use('/api', api.routes(), api.allowedMethods())
 router.use('/admin', admin.routes(), admin.allowedMethods())
+router.use('/wxapi', wxApi.routes(), wxApi.allowedMethods())
 
 module.exports = router
