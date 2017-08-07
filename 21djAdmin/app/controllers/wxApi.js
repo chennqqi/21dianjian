4613c07a7847e8a3eaa456b6cfea630b011ae15f
fujinliang <3487662289@qq.com>
2017-08-07 16:42:00 +0800

    wxApi.js 授权登录

--- 21djAdmin/app/controllers/wxApi.js
+++ 21djAdmin/app/controllers/wxApi.js
@@ -10,7 +10,8 @@ const wxApi = {
     async authLogin(ctx) {
 
         const appID = config.wxAppID
-        const redirect_uri = ctx.request.redirect_uri || 'http%3a%2f%2fwww.yuqianedu.com'
+        const redirect_uri = ctx.request.query.redirect_uri || 'http%3a%2f%2fadmin.didianstudio.com'
+
         const scope = 'snsapi_userinfo'
         const state = constant.AUTH_LOIGN_STATE
         const authUrl = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + appID + '&redirect_uri=' + redirect_uri + '&response_type=code&scope=' + scope + '&state=' + state + '#wechat_redirect'
